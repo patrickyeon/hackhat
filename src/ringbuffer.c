40e9f5fe21879f9c778645f1e64f4530e2b0ee20
Patrick Yeon <patrickyeon@gmail.com>
2018-01-28 19:12:31 -0800

    ringbuffer and some testing to convince myself it works

--- src/ringbuffer.c
+++ src/ringbuffer.c
@@ -4,13 +4,13 @@
 
 int rb_cap(rbuff_t *rb) {
     if (rb->end < rb->start) {
-        return rb->len - (rb->end + rb->len - rb->start);
+        return rb->len - (rb->end + rb->len - rb->start) - 1;
     } else {
-        return rb->len - (rb->end - rb->start);
+        return rb->len - (rb->end - rb->start) - 1;
     }
 }
 
-int rb_push(rbuff_t *rb, char *buff, size_t len) {
+int rb_pop(rbuff_t *rb, char *buff, size_t len) {
     int i;
     for (i = 0; i < len && rb->start != rb->end; i++) {
         buff[i] = rb->buff[rb->start++];
@@ -19,7 +19,7 @@ int rb_push(rbuff_t *rb, char *buff, size_t len) {
     return i;
 }
 
-int rb_pop(rbuff_t *rb, char *buff, size_t len) {
+int rb_push(rbuff_t *rb, char *buff, size_t len) {
     int i;
     for (i = 0; i < len && (rb->end + 1) % rb->len != rb->start; i++) {
         rb->buff[rb->end++] = buff[i];
--- src/ringbuffer.h
+++ src/ringbuffer.h
@@ -12,4 +12,8 @@ int rb_cap(rbuff_t *rb);
 int rb_push(rbuff_t *rb, char *buff, size_t len);
 int rb_pop(rbuff_t *rb, char *buff, size_t len);
 
+#define mk_rb(name, len) \
+    char _##name_buff[len + 1];\
+    rbuff_t  name = {_##name_buff, 0, 0, len + 1};
+
 #endif // RINGBUFFER_H
