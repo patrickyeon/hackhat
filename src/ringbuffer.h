71c0bdf3858f9def35630a7b23034bae49a1456a
Patrick Yeon <patrickyeon@gmail.com>
2018-01-28 19:13:41 -0800

    ringbuffer char -> uint8_t

--- src/ringbuffer.h
+++ src/ringbuffer.h
@@ -1,19 +1,21 @@
 #ifndef RINGBUFFER_H
 #define RINGBUFFER_H
 
+#include <stdint.h>
+
 typedef struct rbuff_t {
-    char *buff;
+    uint8_t *buff;
     size_t start;
     size_t end;
     size_t len;
 } rbuff_t;
 
 int rb_cap(rbuff_t *rb);
-int rb_push(rbuff_t *rb, char *buff, size_t len);
-int rb_pop(rbuff_t *rb, char *buff, size_t len);
+int rb_push(rbuff_t *rb, uint8_t *buff, size_t len);
+int rb_pop(rbuff_t *rb, uint8_t *buff, size_t len);
 
 #define mk_rb(name, len) \
-    char _##name_buff[len + 1];\
+    uint8_t _##name_buff[len + 1];\
     rbuff_t  name = {_##name_buff, 0, 0, len + 1};
 
 #endif // RINGBUFFER_H
--- test/test_ringbuffer.c
+++ test/test_ringbuffer.c
@@ -8,17 +8,17 @@ void test_create_empty(void) {
 
 void test_add_string(void) {
     mk_rb(rb, 64);
-    char str[10] = "hello";
+    uint8_t str[10] = "hello";
     TEST_ASSERT_EQUAL(5, rb_push(&rb, str, 5));
     TEST_ASSERT_EQUAL(64 - 5, rb_cap(&rb));
 }
 
 void test_read_string(void) {
     mk_rb(rb, 64);
-    char str[10] = "hello\0";
+    uint8_t str[10] = "hello\0";
     rb_push(&rb, str, 6);
 
-    char outbuff[10];
+    uint8_t outbuff[10];
     TEST_ASSERT_EQUAL(6, rb_pop(&rb, outbuff, 6));
     TEST_ASSERT_EQUAL_STRING(str, outbuff);
     TEST_ASSERT_EQUAL(0, rb_pop(&rb, outbuff, 6));
@@ -27,30 +27,30 @@ void test_read_string(void) {
 
 void test_do_not_overfill(void) {
     mk_rb(rb, 12);
-    char str[40] = "test string too long\0";
+    uint8_t str[40] = "test string too long\0";
     TEST_ASSERT_EQUAL(12, rb_push(&rb, str, 16));
     TEST_ASSERT_EQUAL(0, rb_cap(&rb));
     TEST_ASSERT_EQUAL(0, rb_push(&rb, str + 4, 12));
     
-    char outstr[12];
+    uint8_t outstr[12];
     TEST_ASSERT_EQUAL(12, rb_pop(&rb, outstr, 12));
     TEST_ASSERT_EQUAL_STRING_LEN(str, outstr, 12);
 }
 
 void test_do_not_overdrain(void) {
     mk_rb(rb, 12);
-    char str[12] = "hello\0";
+    uint8_t str[12] = "hello\0";
     rb_push(&rb, str, 6);
-    char outstr[12] = "\0";
+    uint8_t outstr[12] = "\0";
     TEST_ASSERT_EQUAL(6, rb_pop(&rb, outstr, 10));
     TEST_ASSERT_EQUAL_STRING(str, outstr);
 }
 
 void test_wraps_around(void) {
     mk_rb(rb, 10);
-    char str[12] = "hi mom!\0";
+    uint8_t str[12] = "hi mom!\0";
     rb_push(&rb, str, 8);
-    char outstr[12];
+    uint8_t outstr[12];
     rb_pop(&rb, outstr, 8);
     TEST_ASSERT_EQUAL(8, rb_push(&rb, str, 8));
     TEST_ASSERT_EQUAL(10 - 8, rb_cap(&rb));
